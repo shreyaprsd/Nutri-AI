5da7d9c80c9ebfb60dd640399363fa32bab42883
Shreya Prasad <prasadshreya2303@gmail.com>
2025-11-05 13:55:45 +0530

    Initial Commit

---
+++ Nutri AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nutri AI
+//
+//  Created by Shreya Prasad on 05/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nutri AI/Nutri_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutri_AIApp.swift
+//  Nutri AI
+//
+//  Created by Shreya Prasad on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Nutri_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
